e037f02123b0c4ce75187e5cb427d04df023e605
ben <limhanyangb@gmail.com>
2018-04-23 11:49:56 -0500

    Revert "updated CAN with data structure"
    This reverts commit d76d8430e9306aad104377247cca8ec8f2690b24.

--- main.c
+++ main.c
@@ -68,11 +68,7 @@
  * - What if not connected to UART?
  *
  *
- * MODE:
- * 0. PID tuning
- * 1. PID control
- * 2. Lead-lag tuning
- * 3. Lead-lag control
+ *
  */
 //*****************************************************************************
 
@@ -726,11 +722,10 @@ void tellSlaveAngle(int device, int mode, int angle1, int angle2){
 
 /*
  * This function tells the Master what the hell is going on.
- * Total number of bits: 4+4+16+16 = 40
  */
 void tellMasterAngle(int device,int mode,int angle1,int angle2) {
     ui32MsgDataTX = 0;
-    ui32MsgDataTX = device << 4; // 4 bits
+    ui32MsgDataTX = device<<4; // 8 bits
     ui32MsgDataTX |= mode; // 4 bits
     ui32MsgDataTX =  ui32MsgDataTX << 16; // counts
     ui32MsgDataTX |= angle1;
@@ -832,6 +827,7 @@ void CANget(){
         //
         UARTprintf("Msg ID=0x%08X len=%u data=0x",
                    sCANMessageRX.ui32MsgID, sCANMessageRX.ui32MsgLen);
+<<<<<<< HEAD
         uint8_t device = pui8MsgDataRX[0]&0b11110000; // first 4 bits is address
         uint8_t mode = pui8MsgDataRX[0]&0b00001111;  // next 4 bits is the mode
         uint16_t angle1;
@@ -840,9 +836,12 @@ void CANget(){
             // we are tracking.
             angle1 = (pui8MsgDataRX[1]<<8)|pui8MsgDataRX[2];
             angle2 = (pui8MsgDataRX[3]<<8)|pui8MsgDataRX[4];
+=======
+        for(uIdx = 0; uIdx < sCANMessageRX.ui32MsgLen; uIdx++)
+        {
+            UARTprintf("%02X ", pui8MsgDataRX[uIdx]);
+>>>>>>> parent of d76d843... updated CAN with data structure
         }
-        UARTprintf("Device: %d, Mode: %d, Angle 1:%d, Angle 2:%d \n",device,mode,angle1,angle2);
-
         UARTprintf(" total count=%u\n", g_ui32MsgCount);
     }
     //UARTprintf("Oops, flag not set!\n");
@@ -891,25 +890,25 @@ void CANsend() {
     //ui32MsgDataTX++;
     if (txID==MASTER){
         // tell slaves what to do here
-        tellSlaveAngle(3,1,180,0); // slave 3, mode 1, angle 1 = 180, angle 2 = 0
+        tellSlaveAngle(3,180,0);
     }
     else {
-        // report back to master what we are doing
-        tellMasterAngle(2,1,currPosition0,currPosition1);
+        tellMasterAngle(2,currPosition0,currPosition1);
     }
     //SysCtlDelay(500);
 }
 
 void CANRXmsgconfig(){
-    if (txID==2){ // if I am the controller
+    if (txID==2){
         sCANMessageRX.ui32MsgID = 0;  // accept all incoming data
     }
-    else { // if I am not controller
+    else {
         sCANMessageRX.ui32MsgID = 2;  // accept only commands from 2
     }
+
     sCANMessageRX.ui32MsgIDMask = 0;
     sCANMessageRX.ui32Flags = MSG_OBJ_RX_INT_ENABLE | MSG_OBJ_USE_ID_FILTER;
-    sCANMessageRX.ui32MsgLen = 5;
+    sCANMessageRX.ui32MsgLen = 8;
     CANMessageSet(CAN0_BASE, 2, &sCANMessageRX, MSG_OBJ_TYPE_RX);
 
 }

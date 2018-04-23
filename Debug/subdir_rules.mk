d62f7f7eeb0bb56725299266f51cd22b0fea803f
ben <limhanyangb@gmail.com>
2018-04-23 11:49:30 -0500

    errors like mad

--- Debug/subdir_rules.mk
+++ Debug/subdir_rules.mk
@@ -12,6 +12,41 @@ main.obj: ../main.c $(GEN_OPTS) | $(GEN_HDRS)
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
+rebuscan.obj: ../rebuscan.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: ARM Compiler'
+	"C:/ti/ccsv7/tools/compiler/ti-cgt-arm_17.9.0.STS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="C:/Users/Ben/workspace_v7/qei_mit_pwm" --include_path="C:/ti/TivaWare_C_Series-2.1.3.156" --include_path="C:/ti/ccsv7/tools/compiler/ti-cgt-arm_17.9.0.STS/include" --define=ccs="ccs" --define=PART_TM4C123GH6PM -g --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="rebuscan.d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+rebuspid.obj: ../rebuspid.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: ARM Compiler'
+	"C:/ti/ccsv7/tools/compiler/ti-cgt-arm_17.9.0.STS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="C:/Users/Ben/workspace_v7/qei_mit_pwm" --include_path="C:/ti/TivaWare_C_Series-2.1.3.156" --include_path="C:/ti/ccsv7/tools/compiler/ti-cgt-arm_17.9.0.STS/include" --define=ccs="ccs" --define=PART_TM4C123GH6PM -g --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="rebuspid.d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+rebuspwm.obj: ../rebuspwm.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: ARM Compiler'
+	"C:/ti/ccsv7/tools/compiler/ti-cgt-arm_17.9.0.STS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="C:/Users/Ben/workspace_v7/qei_mit_pwm" --include_path="C:/ti/TivaWare_C_Series-2.1.3.156" --include_path="C:/ti/ccsv7/tools/compiler/ti-cgt-arm_17.9.0.STS/include" --define=ccs="ccs" --define=PART_TM4C123GH6PM -g --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="rebuspwm.d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+rebusqei.obj: ../rebusqei.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: ARM Compiler'
+	"C:/ti/ccsv7/tools/compiler/ti-cgt-arm_17.9.0.STS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="C:/Users/Ben/workspace_v7/qei_mit_pwm" --include_path="C:/ti/TivaWare_C_Series-2.1.3.156" --include_path="C:/ti/ccsv7/tools/compiler/ti-cgt-arm_17.9.0.STS/include" --define=ccs="ccs" --define=PART_TM4C123GH6PM -g --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="rebusqei.d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+rebustimer.obj: ../rebustimer.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: ARM Compiler'
+	"C:/ti/ccsv7/tools/compiler/ti-cgt-arm_17.9.0.STS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="C:/Users/Ben/workspace_v7/qei_mit_pwm" --include_path="C:/ti/TivaWare_C_Series-2.1.3.156" --include_path="C:/ti/ccsv7/tools/compiler/ti-cgt-arm_17.9.0.STS/include" --define=ccs="ccs" --define=PART_TM4C123GH6PM -g --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="rebustimer.d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
 tm4c123gh6pm_startup_ccs.obj: ../tm4c123gh6pm_startup_ccs.c $(GEN_OPTS) | $(GEN_HDRS)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: ARM Compiler'
--- main.c
+++ main.c
@@ -276,8 +276,6 @@ void PortDIntHandler(void){
 
 
 
-// Added to github
-
 //*****************************************************************************
 //
 // A counter that keeps track of the number of times the RX interrupt has
@@ -430,8 +428,6 @@ Timer2IntHandler(void)
 
 }
 
-
-
 void
 UARTIntHandler(void)
 {
@@ -571,6 +567,494 @@ int main(void) {
 
 
 
+
+void PIDVelupdate(){
+
+    currVel1 = QEIVelocityGet(QEI1_BASE);
+    currVel0 = QEIVelocityGet(QEI0_BASE);
+
+    errorv0 = (desiredVel0 - currVel0); // get current error
+    eintv0 = eintv0 + errorv0; // integrate up the error
+    edivv0 = errorv0-eprevv0; // no need to divide by time since you are multiplying by a constant.
+    controlsigv0 = Kp0*errorv0+Kd0*edivv0+Ki0*eintv0;
+
+    if (eintv0>200){
+        eintv0 = 200;
+    }
+    else if(eintv0<-200){
+        eintv0 = -200;
+    }
+
+    if (controlsigv0>100) {
+        pwmoutv0 = 100;
+        dirv0 = 0; // forward full speed
+    }
+    else if(controlsigv0<-100) {
+        dirv0 = 1;
+        pwmoutv0 = 100; // backwards full speed
+    }
+    else if((controlsigv0<0) && (controlsigv0>-100)){
+        pwmoutv0 = -1*controlsigv0; // backwards at controlsig
+        dirv0 = 1;
+    }
+    else if ((controlsigv0>0)&&(controlsigv0<100)){
+        pwmoutv0 = controlsigv0; // forwards at controlsig
+        dirv0 = 0;
+    }
+
+    errorv1 = (desiredVel1 - currVel1); // get current error
+    eintv1 = eintv1 + errorv1; // integrate up the error
+    edivv1 = errorv1-eprevv1; // no need to divide by time since you are multiplying by a constant.
+    controlsigv1 = Kp1*errorv1+Kd1*edivv1+Ki1*eintv1;
+
+    if (eintv1>200){
+        eintv1 = 200;
+    }
+    else if(eintv1<-200){
+        eintv1 = -200;
+    }
+
+    if (controlsigv1>100) {
+        pwmoutv1 = 100;
+        dirv1 = 0; // forward full speed
+    }
+    else if(controlsigv1<-100) {
+        dirv1 = 1;
+        pwmoutv1 = 100; // backwards full speed
+    }
+    else if((controlsigv1<0) && (controlsigv1>-100)){
+        pwmoutv1 = -1*controlsigv1; // backwards at controlsig
+        dirv1 = 1;
+    }
+    else if ((controlsigv1>0)&&(controlsigv1<100)){
+        pwmoutv1 = controlsigv1; // forwards at controlsig
+        dirv1 = 0;
+    }
+    eprev0 = errorv0; // update previous error
+    eprev1 = errorv1; // update previous error
+
+}
+
+void PIDPosupdate(){
+
+    currPosition1 = QEI1posGet(ENCODER_COUNT_1-1);
+    currPosition0 = QEI0posGet(ENCODER_COUNT_1-1);
+
+    error0 = (desiredPos0 - currPosition0); // get current error
+    eint0 = eint0 + error0; // integrate up the error
+    ediv0 = error0-eprev0; // no need to divide by time since you are multiplying by a constant.
+    controlsig0 = Kp0*error0+Kd0*ediv0+Ki0*eint0;
+
+    if (eint0>200){
+        eint0 = 200;
+    }
+    else if(eint0<-200){
+        eint0 = -200;
+    }
+
+    if (controlsig0>100) {
+        pwmout0 = 100;
+        dir0 = 0; // forward full speed
+    }
+    else if(controlsig0<-100) {
+        dir0 = 1;
+        pwmout0 = 100; // backwards full speed
+    }
+    else if((controlsig0<0) && (controlsig0>-100)){
+        pwmout0 = -1*controlsig0; // backwards at controlsig
+        dir0 = 1;
+    }
+    else if ((controlsig0>0)&&(controlsig0<100)){
+        pwmout0 = controlsig0; // forwards at controlsig
+        dir0 = 0;
+    }
+
+    error1 = (desiredPos1 - currPosition1); // get current error
+    eint1 = eint1 + error1; // integrate up the error
+    ediv1 = error1-eprev1; // no need to divide by time since you are multiplying by a constant.
+    controlsig1 = Kp1*error1+Kd1*ediv1+Ki1*eint1;
+
+    if (eint1>200){
+        eint1 = 200;
+    }
+    else if(eint1<-200){
+        eint1 = -200;
+    }
+
+    if (controlsig1>100) {
+        pwmout1 = 100;
+        dir1 = 0; // forward full speed
+    }
+    else if(controlsig1<-100) {
+        dir1 = 1;
+        pwmout1 = 100; // backwards full speed
+    }
+    else if((controlsig1<0) && (controlsig1>-100)){
+        pwmout1 = -1*controlsig1; // backwards at controlsig
+        dir1 = 1;
+    }
+    else if ((controlsig1>0)&&(controlsig1<100)){
+        pwmout1 = controlsig1; // forwards at controlsig
+        dir1 = 0;
+    }
+    eprev0 = error0; // update previous error
+    eprev1 = error1; // update previous error
+
+}
+
+
+/*
+ * This function sets the CAN TX message for the master to tell slave devices the angle to turn to
+ */
+
+void tellSlaveAngle(int device, int mode, int angle1, int angle2){
+    ui32MsgDataTX = 0;
+    ui32MsgDataTX = device<<4; // 8 bits
+    ui32MsgDataTX |= mode; // 4 bits
+    ui32MsgDataTX =  ui32MsgDataTX << 16; // counts
+    ui32MsgDataTX |= angle1;
+    ui32MsgDataTX =  ui32MsgDataTX << 16; // counts
+    ui32MsgDataTX |= angle2;
+    UARTprintf("Argh: %d\n",ui32MsgDataTX);
+    UARTprintf("Size of: %d\n", sizeof(pui8MsgDataTX));
+    int i;
+    for(i = 0; i<sizeof(pui8MsgDataTX);i++){
+        UARTprintf("%d\n",pui8MsgDataTX[i]);
+    }
+
+}
+
+/*
+ * This function tells the Master what the hell is going on.
+ * Total number of bits: 4+4+16+16 = 40
+ */
+void tellMasterAngle(int device,int mode,int angle1,int angle2) {
+    ui32MsgDataTX = 0;
+    ui32MsgDataTX = device << 4; // 4 bits
+    ui32MsgDataTX |= mode; // 4 bits
+    ui32MsgDataTX =  ui32MsgDataTX << 16; // counts
+    ui32MsgDataTX |= angle1;
+    ui32MsgDataTX =  ui32MsgDataTX << 16; // counts
+    ui32MsgDataTX |= angle2;
+}
+
+
+/*
+ * This function sets up the order in which the controllers are ordered.
+ */
+void PULSEorderconfig(){
+    while(interrupt_counter==0){
+        UARTprintf("Waiting for command\n");
+        // do nothing. It will only change if the input GPIO is interrupted OR UART is triggered, then it will be 1.
+    }
+    UARTprintf("Okay, counting!\n");
+    SysCtlDelay(100000);
+    interrupt_counter = interrupt_counter+1; // increment our counter, this is our ID
+    txID = interrupt_counter;
+    UARTprintf("ID is: %d\n", interrupt_counter);
+    PULSEsend();
+    UARTprintf("Sending complete.\n");
+
+}
+
+
+void PULSEconfig(){
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
+    GPIOPinTypeGPIOInput(GPIO_PORTD_BASE, WIRE_GPIO_PIN);  // make F4 an input
+    GPIOPadConfigSet(GPIO_PORTD_BASE,WIRE_GPIO_PIN,GPIO_STRENGTH_2MA,GPIO_PIN_TYPE_STD_WPU);   // enable F4's pullup, the drive strength won't affect the input
+    GPIOIntTypeSet(GPIO_PORTD_BASE,WIRE_GPIO_PIN,GPIO_FALLING_EDGE);
+    GPIOIntRegister(GPIO_PORTD_BASE,PortDIntHandler);
+    GPIOIntEnable(GPIO_PORTD_BASE, WIRE_GPIO_INT_PIN);
+}
+
+void PULSEsend(){
+    // config output here
+    GPIOPinTypeGPIOOutput(GPIO_PORTD_BASE, GPIO_PIN_2);
+    int i;
+    uint32_t state=0;
+    for(i=0;i<interrupt_counter;i++){
+        GPIOPinWrite(GPIO_PORTD_BASE,GPIO_PIN_2, state);
+        SysCtlDelay(1000);
+        state^=GPIO_PIN_2;
+        GPIOPinWrite(GPIO_PORTD_BASE,GPIO_PIN_2, state);
+        SysCtlDelay(1000);
+    }
+    GPIOIntDisable(GPIO_PORTD_BASE,WIRE_GPIO_INT_PIN); // We no longer need the interrupt
+    config_flag=0; // no longer configuring
+}
+
+
+void CANget(){
+
+    unsigned int uIdx;
+
+    //
+    // If the flag is set, that means that the RX interrupt occurred and
+    // there is a message ready to be read from the CAN
+    //
+    if(g_bRXFlag)
+    {
+        // UARTprintf("Function reached\n");
+        //
+        // Reuse the same message object that was used earlier to configure
+        // the CAN for receiving messages.  A buffer for storing the
+        // received data must also be provided, so set the buffer pointer
+        // within the message object.
+        //
+        sCANMessageRX.pui8MsgData = pui8MsgDataRX;
+
+        //
+        // Read the message from the CAN.  Message object number 2 is used
+        // (which is not the same thing as CAN ID).  The interrupt clearing
+        // flag is not set because this interrupt was already cleared in
+        // the interrupt handler.
+        //
+        CANMessageGet(CAN0_BASE, 2, &sCANMessageRX, 0);
+
+        //
+        // Clear the pending message flag so that the interrupt handler can
+        // set it again when the next message arrives.
+        //
+        g_bRXFlag = 0;
+
+
+        //
+        // Check to see if there is an indication that some messages were
+        // lost.
+        //
+        if(sCANMessageRX.ui32Flags & MSG_OBJ_DATA_LOST)
+        {
+            UARTprintf("CAN message loss detected\n");
+        }
+
+        //
+        // Print out the contents of the message that was received.
+        //
+        UARTprintf("Msg ID=0x%08X len=%u data=0x",
+                   sCANMessageRX.ui32MsgID, sCANMessageRX.ui32MsgLen);
+        uint8_t device = pui8MsgDataRX[0]&0b11110000; // first 4 bits is address
+        uint8_t mode = pui8MsgDataRX[0]&0b00001111;  // next 4 bits is the mode
+        uint16_t angle1;
+        uint16_t angle2;
+        if (mode == 1){
+            // we are tracking.
+            angle1 = (pui8MsgDataRX[1]<<8)|pui8MsgDataRX[2];
+            angle2 = (pui8MsgDataRX[3]<<8)|pui8MsgDataRX[4];
+        }
+        UARTprintf("Device: %d, Mode: %d, Angle 1:%d, Angle 2:%d \n",device,mode,angle1,angle2);
+
+        UARTprintf(" total count=%u\n", g_ui32MsgCount);
+    }
+    //UARTprintf("Oops, flag not set!\n");
+
+}
+
+
+void CANsend() {
+    //
+    // Data structure:
+    // Address (to/from)
+    // Command
+    // Data
+    //
+    //UARTprintf("Sending msg: 0x%02X %02X %02X %02X",
+    //           pui8MsgDataTX[0], pui8MsgDataTX[1], pui8MsgDataTX[2],
+    //           pui8MsgDataTX[3]);
+    //UARTprintf("data is %d",ui32MsgDataTX);
+    //
+
+
+
+    // Send the CAN message using object number 1 (not the same thing as
+    // CAN ID, which is also 1 in this example).  This function will cause
+    // the message to be transmitted right away.
+    CANMessageSet(CAN0_BASE, 1, &sCANMessageTX, MSG_OBJ_TYPE_TX);
+    //
+    // Check the error flag to see if errors occurred
+    //
+    if(g_bErrFlag)
+    {
+        UARTprintf(" error - cable connected?\n");
+    }
+    else
+    {
+        //
+        // If no errors then print the count of message sent
+        //
+        //UARTprintf(" total count = %u\n", g_ui32MsgCount);
+    }
+
+    //
+    // Increment the value in the message data.
+    //
+
+    //ui32MsgDataTX++;
+    if (txID==MASTER){
+        // tell slaves what to do here
+        tellSlaveAngle(3,1,180,0); // slave 3, mode 1, angle 1 = 180, angle 2 = 0
+    }
+    else {
+        // report back to master what we are doing
+        tellMasterAngle(2,1,currPosition0,currPosition1);
+    }
+    //SysCtlDelay(500);
+}
+
+void CANRXmsgconfig(){
+    if (txID==2){ // if I am the controller
+        sCANMessageRX.ui32MsgID = 0;  // accept all incoming data
+    }
+    else { // if I am not controller
+        sCANMessageRX.ui32MsgID = 2;  // accept only commands from 2
+    }
+    sCANMessageRX.ui32MsgIDMask = 0;
+    sCANMessageRX.ui32Flags = MSG_OBJ_RX_INT_ENABLE | MSG_OBJ_USE_ID_FILTER;
+    sCANMessageRX.ui32MsgLen = 5;
+    CANMessageSet(CAN0_BASE, 2, &sCANMessageRX, MSG_OBJ_TYPE_RX);
+
+}
+
+void CANTXmsgconfig(uint8_t *pui8MsgData){
+    sCANMessageTX.ui32MsgID = txID;
+    sCANMessageTX.ui32MsgIDMask = 0;
+    sCANMessageTX.ui32Flags = MSG_OBJ_TX_INT_ENABLE;
+    sCANMessageTX.ui32MsgLen = sizeof(pui8MsgDataTX);
+    sCANMessageTX.pui8MsgData = pui8MsgDataTX;
+    CANMessageSet(CAN0_BASE, 1, &sCANMessageTX, MSG_OBJ_TYPE_TX);
+
+}
+
+void CANconfig(){
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+    GPIOPinTypeGPIOOutput(GPIO_PORTB_BASE,GPIO_PIN_6); // standby pin
+    GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_6, 0); // HIGH = standby, LOW = active
+
+    GPIOPinConfigure(GPIO_PB4_CAN0RX);
+    GPIOPinConfigure(GPIO_PB5_CAN0TX);
+    GPIOPinTypeCAN(GPIO_PORTB_BASE, GPIO_PIN_4 | GPIO_PIN_5);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_CAN0);
+    CANInit(CAN0_BASE);
+    //CANRetrySet(CAN0_BASE,1);
+    // In this example, the CAN bus is set to 500 kHz.
+    if(CANBitRateSet(CAN0_BASE, SysCtlClockGet(), 1000000)==0){
+        while(1){
+            UARTprintf("Bit rate set error!");
+        }
+    }
+    CANIntEnable(CAN0_BASE, CAN_INT_MASTER | CAN_INT_ERROR | CAN_INT_STATUS);
+    IntEnable(INT_CAN0);
+    CANEnable(CAN0_BASE);
+
+
+
+}
+
+float counttoRPM(int count){
+     return (float)count/(COUNT_PER_REV*TIME_TO_COUNT);
+}
+
+void setSpeed(int pwm, int state, int motor){ // 0 to CW, // 1 to CCW // 2 to brake // 4 to stop
+    if (motor==0){ // motor 1
+        PWMPulseWidthSet(PWM1_BASE, PWM_OUT_5,pwm);
+        GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_0|GPIO_PIN_1,stateTable[state]);
+    }
+    else if (motor == 1){
+        PWMPulseWidthSet(PWM1_BASE, PWM_OUT_6,pwm);
+        GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_2|GPIO_PIN_3,stateTable[state]<<2);
+    }
+}
+
+int angleToPosition(int angle){
+    return (int)((float)angle/360.0*COUNT_PER_REV);
+}
+
+int positionToAngle(int position){
+    return (int)((float)position/(float)COUNT_PER_REV*360);
+}
+
+int QEI0angleGet(int max_count){
+    return (int)((float)QEI0posGet(max_count)/(float)max_count*360.0*5);
+}
+
+int QEI0posGet(int max_count){
+   int curr_pos = QEIPositionGet(QEI0_BASE);
+   curr_pos = curr_pos - max_count/2;
+   return curr_pos;
+}
+
+void QEI0zero(int max_count){
+    QEIPositionSet(QEI0_BASE,max_count/2);
+}
+
+
+int QEI1angleGet(int max_count){
+    int some_val = (int)(((float)QEI1posGet(max_count)/(float)max_count)*360.0*5);
+    return some_val;
+
+}
+
+int QEI1posGet(int max_count){
+   int curr_pos = QEIPositionGet(QEI1_BASE);
+   curr_pos = curr_pos - max_count/2;
+   return curr_pos;
+}
+
+void QEI1zero(int max_count){
+    QEIPositionSet(QEI1_BASE,max_count/2);
+}
+
+void QEIconfig(int limit1, int limit2){
+    // QEI Config
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD); // We are using PD7 for PhB0, QEI module 0 phase B
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC); // PC5 - A,PC6 - B
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_QEI0);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_QEI1);
+
+    //Unlock GPIOD7 - Like PF0 its used for NMI - Without this step it doesn't work
+    HWREG(GPIO_PORTD_BASE + GPIO_O_LOCK) = GPIO_LOCK_KEY; //In Tiva include this is the same as "_DD" in older versions (0x4C4F434B)
+    HWREG(GPIO_PORTD_BASE + GPIO_O_CR) |= 0x80;
+    HWREG(GPIO_PORTD_BASE + GPIO_O_LOCK) = 0;
+
+    //Set Pins to be PHA0 and PHB0
+    GPIOPinConfigure(GPIO_PD6_PHA0); // QEI module 0 phase A
+    GPIOPinConfigure(GPIO_PD7_PHB0); // QEI module 0 phase B
+
+    //Set Pins to be PHA1 and PHB1
+    GPIOPinConfigure(GPIO_PC5_PHA1); // QEI module 1 phase A
+    GPIOPinConfigure(GPIO_PC6_PHB1); // QEI module 1 phase B
+
+    //Set GPIO pins for QEI. PhA0 -> PD6, PhB0 ->PD7. I believe this sets the pull up and makes them inputs
+    GPIOPinTypeQEI(GPIO_PORTD_BASE, GPIO_PIN_6 |  GPIO_PIN_7);
+    GPIOPinTypeQEI(GPIO_PORTC_BASE, GPIO_PIN_5 |  GPIO_PIN_6);
+
+    //Disable peripheral and int before configuration
+    QEIDisable(QEI0_BASE);
+
+    QEIIntDisable(QEI0_BASE,QEI_INTERROR | QEI_INTDIR | QEI_INTTIMER | QEI_INTINDEX);
+    // Configure quadrature encoder, use an arbitrary top limit of 1000
+    QEIConfigure(QEI0_BASE, (QEI_CONFIG_CAPTURE_A_B  | QEI_CONFIG_NO_RESET  | QEI_CONFIG_QUADRATURE | QEI_CONFIG_NO_SWAP), limit1);
+    // Enable the quadrature encoder.
+    QEIEnable(QEI0_BASE);
+    //Set position to a middle value so we can see if things are working
+    QEIPositionSet(QEI0_BASE, limit1/2);
+
+    QEIDisable(QEI1_BASE);
+    QEIIntDisable(QEI1_BASE,QEI_INTERROR | QEI_INTDIR | QEI_INTTIMER | QEI_INTINDEX);
+    // Configure quadrature encoder, use an arbitrary top limit of 1000
+    QEIConfigure(QEI1_BASE, (QEI_CONFIG_CAPTURE_A_B  | QEI_CONFIG_NO_RESET  | QEI_CONFIG_QUADRATURE | QEI_CONFIG_NO_SWAP), limit2);
+    // Enable the quadrature encoder.
+    QEIEnable(QEI1_BASE);
+    //Set position to a middle value so we can see if things are working
+    QEIPositionSet(QEI1_BASE, limit2/2);
+}
+
+void QEIvelocityConfig(){
+    QEIVelocityConfigure(QEI0_BASE,QEI_VELDIV_1,SysCtlClockGet()*TIME_TO_COUNT); // base, predivider, clock ticks to count for
+    QEIVelocityConfigure(QEI1_BASE,QEI_VELDIV_1,SysCtlClockGet()*TIME_TO_COUNT);
+    QEIVelocityEnable(QEI0_BASE);
+    QEIVelocityEnable(QEI1_BASE);
+    // QEIVelocityGet(QEI0_BASE); // returns the number of pulses captured in the time period
+}
 
 
 
